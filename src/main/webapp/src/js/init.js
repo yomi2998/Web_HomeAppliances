fd69f41b62a445c5ee190ae020b5083b43bc460d
yomi2998 <130840857+yomi2998@users.noreply.github.com>
2024-03-18 04:07:22 +0800

    nuke ul

--- src/main/webapp/src/js/init.js
+++ src/main/webapp/src/js/init.js
@@ -3,7 +3,3 @@ document.addEventListener("DOMContentLoaded", function () {
     //init
     
 });
-
-window.addEventListener('beforeunload', function (event) {
-    document.body.classList.add('fade-out');
-});
