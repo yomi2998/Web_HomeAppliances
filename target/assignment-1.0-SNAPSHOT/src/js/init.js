d8a223093f067ca0384bf1ce83b6cb410b286324
yomi2998 <130840857+yomi2998@users.noreply.github.com>
2024-03-18 03:39:32 +0800

    some transition on anchors

--- target/assignment-1.0-SNAPSHOT/src/js/init.js
+++ target/assignment-1.0-SNAPSHOT/src/js/init.js
@@ -3,3 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //init
     
 });
+
+window.addEventListener('beforeunload', function (event) {
+    document.body.classList.add('fade-out');
+});
