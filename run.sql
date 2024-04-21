24028b24995940a2f01068505a946594bf269975
yomi2998 <130840857+yomi2998@users.noreply.github.com>
2024-04-22 01:33:59 +0800

    database tables

--- run.sql
+++ run.sql
@@ -1,24 +1,174 @@
 CREATE TABLE customer (
-    id VARCHAR(10) PRIMARY KEY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
     name VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    address VARCHAR(255) NOT NULL,
     balance DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    birth_date DATE NOT NULL,
     join_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO customer (id, name, email, password, phone, address) VALUES ('C001', 'John Doe', 'example@mail.com', 'password', '1234567890', '123 Main St, New York, NY');
+CREATE TABLE shipping_details (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    user_id INT NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    recipient_name VARCHAR(100) NOT NULL,
+    contact_number VARCHAR(20) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES customer(id) ON DELETE CASCADE,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE admin (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO admin (password) VALUES ('$nelson1234');
+
+CREATE TABLE staff (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    name VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    contact_number VARCHAR(20) NOT NULL,
+    join_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE category (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    name VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE product (
-    id VARCHAR(10) PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    image VARCHAR(255) NOT NULL,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    name VARCHAR(255) NOT NULL,
+    display_image_url VARCHAR(255) NOT NULL,
+    description LONG VARCHAR NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     stock INT NOT NULL,
+    category_id INT,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
+);
+
+CREATE TABLE product_image (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    product_id INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
+);
+
+CREATE TABLE product_rating (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    product_id INT NOT NULL,
+    user_id INT,
+    rating INT NOT NULL,
     description LONG VARCHAR NOT NULL,
-    rating DECIMAL(2, 1) NOT NULL DEFAULT 0.0,
-    purchase_count INT NOT NULL DEFAULT 0,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES customer(id) ON DELETE SET NULL
+);
+
+CREATE TABLE product_rating_image (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    product_rating_id INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (product_rating_id) REFERENCES product_rating(id) ON DELETE CASCADE
+);
+
+CREATE TABLE card ( -- for user
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    user_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    card_number VARCHAR(20) NOT NULL,
+    expiry_date TIMESTAMP NOT NULL,
+    cvv VARCHAR(3) NOT NULL,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES customer(id) ON DELETE CASCADE
+);
+
+CREATE TABLE cart (
+    user_id INT NOT NULL,
+    product_id INT,
+    quantity INT NOT NULL,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES customer(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE SET NULL --notify user that product is no longer available then remove from cart
+);
+
+CREATE TABLE discount (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    code VARCHAR(20) NOT NULL,
+    static_value BOOLEAN NOT NULL DEFAULT FALSE,
+    value DECIMAL(10, 2) NOT NULL,
+    min_spend DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    enforce_category BOOLEAN NOT NULL DEFAULT FALSE,
+    category_id INT,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    start_date DATE NOT NULL,
+    expiry_date DATE NOT NULL,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
+);
+
+CREATE TABLE cust_order (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    user_id INT,
+    shipping_details_id INT,
+    total_price DECIMAL(10, 2) NOT NULL,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    delete_shipping_on_complete BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES customer(id) ON DELETE NO ACTION,
+    FOREIGN KEY (shipping_details_id) REFERENCES shipping_details(id) ON DELETE NO ACTION
+);
+
+CREATE TABLE order_product ( -- this is where u can get amount of discount applied/total product price
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    order_id INT NOT NULL,
+    discount_id INT NOT NULL DEFAULT 0, -- 0 = No discount
+    product_id INT,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES cust_order(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE SET NULL,
+    FOREIGN KEY (discount_id) REFERENCES discount(id)
+);
+
+CREATE TABLE order_status (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    order_id INT NOT NULL,
+    status LONG VARCHAR NOT NULL,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES cust_order(id) ON DELETE CASCADE
+);
+
+CREATE TABLE topup (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    user_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    is_refund BOOLEAN NOT NULL DEFAULT FALSE,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES customer(id) ON DELETE CASCADE
+);
+
+CREATE TABLE feedback (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    user_id INT NOT NULL,
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_closed BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES customer(id) ON DELETE CASCADE
+);
+
+CREATE TABLE feedback_message (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY(Start with 1, Increment by 1),
+    feedback_id INT NOT NULL,
+    message LONG VARCHAR NOT NULL,
+    is_staff BOOLEAN NOT NULL DEFAULT FALSE,
+    staff_id INT DEFAULT 0, -- 0 = Admin
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (feedback_id) REFERENCES feedback(id) ON DELETE CASCADE,
+    FOREIGN KEY (staff_id) REFERENCES staff(id) ON DELETE SET NULL
 );
