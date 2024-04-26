5c2a4ddd261b000424168aa84f01ff08b583436c
xenonlds <163749798+xenonlds@users.noreply.github.com>
2024-04-26 15:27:05 +0800

    Footer

---
+++ web/Java/domain/payment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src.domain;
+
+import java.util.Objects;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Phuah
+ */
+public class payment implements Serializable{
+    private String code;
+}
